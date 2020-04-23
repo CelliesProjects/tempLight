2382bd1b3183dbd6cffb3d3a992302b2db617c95
Cellie <CelliesProjects@users.noreply.github.com>
2020-04-23 16:14:33 +0200

    SSD1306 display support.
    Getting temperature from the host runs in a separate task.
    
    Color control by temp finally works.
    
    Save state stuff to NVS.

--- tempLight.ino
+++ tempLight.ino
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 #include<WiFi.h>
+#include <Wire.h>
+#include <SSD1306.h>
 #include<Preferences.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include "index_htm.h"
 #include "lightState.h"
-#define NUMBER_OF_CHANNELS 3
 
-#define SET_STATIC_IP false                              /* If SET_STATIC_IP is set to true then STATIC_IP, GATEWAY, SUBNET and PRIMARY_DNS have to be set to some sane values */
 const char * SSID = "huiskamer";
 const char * PSK = "0987654321";
+
+#define SET_STATIC_IP false                              /* If SET_STATIC_IP is set to true then STATIC_IP, GATEWAY, SUBNET and PRIMARY_DNS have to be set to some sane values */
 
 const IPAddress STATIC_IP(192, 168, 0, 60);              /* This should be outside your router dhcp range! */
 const IPAddress GATEWAY(192, 168, 0, 1);                 /* Set to your gateway ip address */
@@ -17,16 +19,34 @@ const IPAddress SUBNET(255, 255, 255, 0);                /* Usually 255,255,255,
 const IPAddress PRIMARY_DNS(192, 168, 0, 30);            /* Check in your router */
 const IPAddress SECONDARY_DNS(192, 168, 0, 50);          /* Check in your router */
 
+#define NUMBER_OF_CHANNELS 3
+#define COLOR_STR_LENGTH 8    /* "#ff00ff" is 7 long + 1 extra pos for terminating zero */
+#define STROBO_DEFAULT_HZ 5
+
+#define I2C_SCL_PIN                         19
+#define I2C_SDA_PIN                         23
+#define OLED_ADDRESS                        0x3C
+
+
 const uint8_t pin[NUMBER_OF_CHANNELS] {22, 21, 17};
 
-struct ledChannel_t {
-  uint32_t val;
-};
+const char *   TEMPERATURE_HOST_NAME      = "192.168.0.80";
+const uint16_t TEMPERATURE_HOST_PORT      = 80;
+const char *   TEMPERATURE_HOST_URL       = "/api/getdevice?status=";
+const char *   SENSOR_NAME                = "kamer";
 
-#define COLOR_STR_LENGTH 8    /* example: "#ff0000" for red */
+const char *   NVS_NAMESPACE              = "tempLight";
+const char *   NVS_KEY_FREQ               = "freq";
+const char *   NVS_KEY_COLOR              = "color";
+const char *   NVS_KEY_LIGHTSTATE         = "lightstate";
+
+struct temperature_t {
+  float temperature;
+  char color[COLOR_STR_LENGTH];
+} temperature;
 
 struct color_t {
-  char color[COLOR_STR_LENGTH];
+  char color[COLOR_STR_LENGTH]; /* example: "#ff0000" for red */
 } color;
 
 struct strobo_t {
@@ -34,14 +54,12 @@ struct strobo_t {
   char color[COLOR_STR_LENGTH]; /* example: "#ff0000" for red */
 } stroboscope;
 
-Preferences preferences;
-ledChannel_t led[NUMBER_OF_CHANNELS];
-
-#define DEFAULT_PRIORITY 5
-
+SSD1306 OLED(OLED_ADDRESS, I2C_SDA_PIN, I2C_SCL_PIN);
 AsyncWebServer http(80);
 AsyncWebSocket ws("/ws");
 lightState light;
+Preferences preferences;
+bool oledFound{false};
 
 double setLedc(uint32_t frequency, uint8_t numberOfBits) {
   double retfreq{0};
@@ -56,6 +74,9 @@ double setLedc(uint32_t frequency, uint8_t numberOfBits) {
   return retfreq;
 }
 
+inline float mapFloat(const float &x, const float &in_min, const float &in_max, const float &out_min, const float &out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 void writeHexToLedc(const char * color) {
   uint32_t tmp = strtol(&color[1], NULL, 16);
@@ -67,10 +88,39 @@ void writeHexToLedc(const char * color) {
   ledcWrite(2, map(blueVal, 0, 255, 0, 65535));
 }
 
+void updateOLED() {
+  if (!oledFound) return;
+  OLED.clear();
+  OLED.setFont(ArialMT_Plain_16);
+  OLED.drawString(64, 0, WiFi.localIP().toString());
+  OLED.drawString(64, 16 + 24, light.stateStr(light.state()));
+  OLED.setFont(ArialMT_Plain_24);
+  OLED.drawString(64, 16, (String)temperature.temperature+"℃");
+  OLED.display();
+}
+
 void setup() {
   ESP_LOGI(TAG, "tempLight v0.0");
-  preferences.begin("tempLight", false);
-  preferences.getBytes("lastColor", color.color, sizeof(color.color));
+  preferences.begin(NVS_NAMESPACE, false);
+  preferences.getBytes(NVS_KEY_COLOR, color.color, sizeof(color.color));
+  lightState_t state = (lightState_t)preferences.getUInt(NVS_KEY_LIGHTSTATE, LS_OFF);
+  ESP_LOGI(TAG, "Restoring state to '%s'", light.stateStr(state));
+  light.set(state);
+
+  stroboscope.freq = preferences.getULong(NVS_KEY_FREQ, STROBO_DEFAULT_HZ);
+
+  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN, 400000);
+  Wire.beginTransmission(OLED_ADDRESS);
+  uint8_t error = Wire.endTransmission();
+  if (error) ESP_LOGI(TAG, "No SSD1306 OLED found.");
+  else {
+    OLED.init();
+    OLED.flipScreenVertically();
+    OLED.setTextAlignment(TEXT_ALIGN_CENTER);
+    ESP_LOGI(TAG, "Found SSD1306 OLED on I2C at address 0x%x.", OLED_ADDRESS);
+    oledFound = true;
+  }
+
   if (SET_STATIC_IP && !WiFi.config(STATIC_IP, GATEWAY, SUBNET, PRIMARY_DNS, SECONDARY_DNS)) ESP_LOGE(TAG, "Setting static IP failed");
   WiFi.mode(WIFI_STA);
   WiFi.setSleep(false);
@@ -105,12 +155,21 @@ void setup() {
   if (!setLedc(1300, 16)) {
     ESP_LOGE(TAG, "Invalid frequency/bitdepth");
   }
+  //spin up a task that updates the temp every second
+  xTaskCreatePinnedToCore(
+    temperatureTask,                /* Function to implement the task */
+    "tempTask",                     /* Name of the task */
+    3000,                           /* Stack size in words */
+    NULL,                           /* Task input parameter */
+    1,                              /* Priority of the task */
+    NULL,                           /* Task handle. */
+    1);
+
 }
 
 void loop() {
   switch (light.state()) {
     case LS_OFF: {
-        ESP_LOGI(TAG, "Switching to off");
         ledcWrite(0, 0);
         ledcWrite(1, 0);
         ledcWrite(2, 0);
@@ -120,46 +179,31 @@ void loop() {
         break;
       }
     case LS_TEMPERATURE: {
+        if (!setLedc(1300, 16)) {
+          ESP_LOGE(TAG, "Invalid frequency/bitdepth");
+        }
         while (light.state() == LS_TEMPERATURE) {
-          const uint16_t port = 80;
-          const char * host = "192.168.0.80"; // ip or dns
-          ESP_LOGD(TAG, "Connecting to %s:%i", host, port);
-          WiFiClient client;
-          if (!client.connect(host, port)) {
-            ESP_LOGE(TAG, "Could not connect to %s:%i", host, port);
-            while (light.state() == LS_TEMPERATURE) {
-              delay(10);
-            }
-          }
-          else {
-            ESP_LOGD(TAG, "Connected to %s:%i", host, port);
-            client.print("GET /api/getdevice?status= HTTP/1.1\n\n");
-          }
+          const uint32_t LOWTEMP = 15;
+          const uint32_t NICETEMP = 20;
+          const uint32_t HIGHTEMP = 25;
+          //calculate R,G,B values to put in temperature.color
+          uint32_t red, green, blue;
+          // red
+          if (temperature.temperature < NICETEMP) red = 0;
+          else if (temperature.temperature > HIGHTEMP) red = 255;
+          else red = mapFloat(temperature.temperature, NICETEMP, HIGHTEMP, 0, 255);
+          // blue
+          if (temperature.temperature < LOWTEMP) blue = 255;
+          else if (temperature.temperature > NICETEMP) blue = 0;
+          else blue = mapFloat(temperature.temperature, LOWTEMP, NICETEMP, 255, 0);
+          // green
+          if (temperature.temperature < LOWTEMP || temperature.temperature > HIGHTEMP) green = 0;
+          else if (temperature.temperature > LOWTEMP && temperature.temperature < NICETEMP) green = mapFloat(temperature.temperature, LOWTEMP, NICETEMP, 0, 255);
+          else green = mapFloat(temperature.temperature, NICETEMP, HIGHTEMP, 255, 0);
 
-          int maxloops = 0;
-          while (!client.available() && maxloops < 1000 && light.state() == LS_TEMPERATURE) {
-            maxloops++;
-            delay(1); //delay 1 msec
-          }
-
-          bool sensorFound{false};
-          if (client.available() > 0) {
-            while (client.available() && !sensorFound && light.state() == LS_TEMPERATURE) {
-              String line = client.readStringUntil('\r\n');
-              ESP_LOGD(TAG, "Read:%s", line.c_str());
-              if (line.startsWith("kamer")) {
-                float temp = atof(line.substring(6).c_str());
-                ESP_LOGI(TAG, "found sensor:%f", temp);
-                ws.printfAll("sensor\n%.1f", temp);
-                sensorFound = true;
-              }
-            }
-          }
-          else {
-            ESP_LOGI(TAG, "client.available() timed out ");
-          }
-          if (client) client.stop();
-          delay(1000);
+          snprintf(temperature.color, sizeof(temperature.color), "#%02x%02x%02x", red, green, blue);
+          writeHexToLedc(temperature.color);
+          delay(1);
         }
         break;
       }
@@ -167,10 +211,18 @@ void loop() {
         if (!setLedc(1300, 16)) {
           ESP_LOGE(TAG, "Invalid frequency/bitdepth");
         }
+        writeHexToLedc(color.color);
+        char oldHex[COLOR_STR_LENGTH];
+        preferences.getBytes(NVS_KEY_COLOR, oldHex, COLOR_STR_LENGTH);
         while (light.state() == LS_COLOR) {
-          writeHexToLedc(color.color);
+          if (strcmp(color.color, oldHex)) {
+            writeHexToLedc(color.color);
+            snprintf((char*)oldHex, COLOR_STR_LENGTH, color.color);
+            ESP_LOGI(TAG, "Saving %s to prefs", color.color);
+            preferences.putBytes(NVS_KEY_COLOR, (char*)color.color, sizeof(color.color));
+          }
+          delay(1);
         }
-        preferences.putBytes("lastColor", (char*)color.color, sizeof(color.color));
         break;
       }
     case LS_STROBO: {
@@ -184,23 +236,19 @@ void loop() {
               ledcWrite(0, 65535 / 2);
               ledcWrite(1, 65535 / 2);
               ledcWrite(2, 65535 / 2);
+              preferences.putULong(NVS_KEY_FREQ, stroboscope.freq);
             }
           }
           delay(1);
         }
         break;
       }
-    /*
-      case LS_COLORFADE : {}
-      break;
-      case LS_HEARTBEAT : {}
-      break;
-    */
     default: {
         ESP_LOGE(TAG, "ERROR! Request for unhandled lightState->%i", light.state());
         break;
       }
   }
+  updateOLED();
 }
 
 
@@ -208,9 +256,11 @@ void loop() {
 
 void onEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
   if (type == WS_EVT_CONNECT) {
-    client->printf("state\n%s", light.stateStr(light.state()));
     client->printf("strobofreq\n%.2f", stroboscope.freq);
     client->printf("colorname\n%s", color.color);
+    client->printf("sensor\n%f", temperature.temperature);
+    client->printf("state\n%s", light.stateStr(light.state()));
+    return;
   }
 
   else if (type == WS_EVT_DATA) {
@@ -219,73 +269,116 @@ void onEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventTyp
       if (info->opcode == WS_TEXT) {
         data[len] = 0;
         char *pch = strtok((char*)data, "\n");
+
         //pch points to command
-        if (!strcmp("new_strobofreq", pch)) {
+        if (!strcmp("setstrobofreq", pch)) {
           pch = strtok(NULL, "\n");
           //check for valid freq
-          stroboscope.freq = atof(pch);
-          ESP_LOGD(TAG, "New strobofreq received: %.2f", stroboscope.freq);
-          client->printf("strobofreq\n%.2f", stroboscope.freq);
+          if (atof(pch) > 0 && atof(pch) < 100) {
+            stroboscope.freq = atof(pch);
+            ESP_LOGD(TAG, "New strobofreq received: %.2f", stroboscope.freq);
+            client->printf("strobofreq\n%.2f", stroboscope.freq);
+          }
+          else
+            client->printf("error\ninvalid stroboscope freq %i", stroboscope.freq);
           return;
         }
-        /*
-          else if (!strcmp("strobofreq", pch)) {
-          //pch = strtok(NULL, "\n");
-          ESP_LOGI(TAG, "Request for strobofreq received: %s", pch);
-          client->printf("strobofreq\n%i", stroboscope.freq);
-          return;
-          }
-        */
 
         else if (!strcmp("color", pch)) {
           pch = strtok(NULL, "\n");
           ESP_LOGD(TAG, "Request for color received: %s", pch);
           // check for valid color
           snprintf(color.color, sizeof(color.color), pch);
-          client->printf("color\n%i", color.color);
-          return;
-        }
-
-        else if (!strcmp("strobo_color", pch)) {
-          pch = strtok(NULL, "\n");
-          ESP_LOGD(TAG, "Request for strobocolor received: %s", pch);
-          // check for valid color
-          snprintf(stroboscope.color, sizeof(stroboscope.color), pch);
-          client->printf("strobocolor\n%i", stroboscope.color);
           return;
         }
 
         else if (!strcmp("switchstate", pch)) {
           pch = strtok(NULL, "\n");
-          ESP_LOGI(TAG, "Request for new state received: %s", pch);
+          lightState_t oldState = light.state();
+          ESP_LOGD(TAG, "Request for state switch received: %s", pch);
           /* find out which state is requested */
-          if (!strcmp("OFF", pch)) {
+          if (!strcmp("OFF", pch) && light.state() != LS_OFF) {
             light.set(LS_OFF);
-            return;
+            //return;
           }
-          else if (!strcmp("TEMPERATURE", pch)) {
+          else if (!strcmp("TEMPERATURE", pch) && light.state() != LS_TEMPERATURE) {
             light.set(LS_TEMPERATURE);
-            return;
           }
-          else if (!strcmp("COLOR", pch)) {
+          else if (!strcmp("COLOR", pch) && light.state() != LS_COLOR) {
             light.set(LS_COLOR);
-            return;
           }
-          else if (!strcmp("STROBOSCOPE", pch)) {
-            ESP_LOGI(TAG, "  state:%s", pch);
+          else if (!strcmp("STROBOSCOPE", pch) && light.state() != LS_STROBO) {
+            //ESP_LOGI(TAG, "  state:%s", pch);
             light.set(LS_STROBO);
-            return;
           }
+          //we are done with the handlers so check if we need to save the new state to NVS
+          if (light.state() != oldState) {
+            ESP_LOGI(TAG, "Saving light state %s", light.stateStr(light.state()));
+            preferences.putUInt(NVS_KEY_LIGHTSTATE, light.state());
+            oldState = light.state();
+          }
+          return;
         } /*switchstate */
 
-
-
-
-
-        else {                                                     /*unhandled requests end up here */
-          ESP_LOGI(TAG, "Could not handle: '%s'", pch);
+        else {                                                     /*unhandled commands end up here */
+          ESP_LOGI(TAG, "ERROR! Unknown command: '%s'", pch);
         }
       }
     }
   }
 }
+
+void temperatureTask(void * param) {
+  ESP_LOGD(TAG, "Connecting to %s:%i", TEMPERATURE_HOST_NAME, TEMPERATURE_HOST_PORT);
+  WiFiClient client;
+  while (1) {
+    if (!client.connect(TEMPERATURE_HOST_NAME, TEMPERATURE_HOST_PORT)) {
+      ESP_LOGE(TAG, "Could not connect to %s:%i", TEMPERATURE_HOST_NAME, TEMPERATURE_HOST_PORT);
+      while (1) {  //TODO: wait a bit and retry?
+        delay(10);
+      }
+    }
+    else {
+      ESP_LOGD(TAG, "Connected to %s:%i", TEMPERATURE_HOST_NAME, TEMPERATURE_HOST_PORT);
+      client.printf("GET %s HTTP/1.1\n\n", TEMPERATURE_HOST_URL);
+    }
+
+    int maxloops = 0;
+    while (!client.available() && maxloops < 1000) {
+      maxloops++;
+      delay(1); //delay 1 msec
+    }
+
+    bool sensorFound{false};
+    if (client.available() > 0) {
+      while (client.available() && !sensorFound) {
+        String line = client.readStringUntil((char)'\r\n');
+        ESP_LOGD(TAG, "Read:%s", line.c_str());
+        if (line.startsWith(SENSOR_NAME)) {
+          temperature.temperature = atof(line.substring(sizeof(SENSOR_NAME) + 2).c_str());
+          ESP_LOGD(TAG, "found sensor:%s %f", SENSOR_NAME, temperature);
+          static float previousTemperature;
+          if (previousTemperature != temperature.temperature) {
+            ws.printfAll("sensor\n%f", temperature.temperature);
+            previousTemperature = temperature.temperature;
+            updateOLED();
+
+
+
+
+
+          }
+          sensorFound = true;
+        }
+      }
+      if (!sensorFound) {
+        ESP_LOGE(TAG, "Sensor %s was not found.", SENSOR_NAME);
+      }
+    }
+    else {
+      ESP_LOGE(TAG, "client.available() timed out ");
+    }
+    if (client) client.stop();
+    delay(1000);
+  }
+} /*temperatureTask */
